00253cff93a204f6bfe8f5f88193ba89a60d8ab9
Maick Souza <maick_a_s@msn.com>
2020-09-12 17:10:11 -0300

    style(web): finalized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 
 import RocketseatLogo from '../assets/rocketseat.svg'
+import { Container } from '../styles/pages/Index'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Testando 123</title>
       </Head>
 
-      <main>
-        <RocketseatLogo />
-        <h1>Maick Souza</h1>
-      </main>
-    </div>
+      <RocketseatLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + NextJS Structure made by Rocketseat.</p>
+    </Container>
   )
 }
 
