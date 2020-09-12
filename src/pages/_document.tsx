0ee2a6960836af9f2c2d8eb41ce31c244f9bd823
Maick Souza <maick_a_s@msn.com>
2020-09-12 16:48:31 -0300

    style(web): responsive #3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Main,
+  NextScript,
+  Head
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -29,4 +36,27 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="https://rocketseat.com.br/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+// 29'16 ,parei <aqui className=""></aqui>
