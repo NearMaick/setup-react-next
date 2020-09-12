641ec947f0da4c1a48e8b43fefc817c5e3179d63
Maick Souza <maick_a_s@msn.com>
2020-09-12 13:03:38 -0300

    chore(web): initial settings

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Testando 123</title>
+      </Head>
+
+      <main>
+        <h1>Maick Souza</h1>
+      </main>
+    </div>
+  )
+}
